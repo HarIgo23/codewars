6c50ddaed9eaf6a70c3a98b12d740f127e533250
HarIgo23 <igorn2304@gmail.com>
2020-10-13 16:40:25 +0300

    refactor loop and if/else

--- 4 kyu/Tournament Cross Table with Sonneborn-Berger Score.py
+++ 4 kyu/Tournament Cross Table with Sonneborn-Berger Score.py
@@ -3,9 +3,8 @@
 
 def crosstable(players, results):
     count = len(players)
-    pts = [sum(res[:i] + res[i+1:]) for res, i in zip(results, range(count))]
-    sb = [res[i] * pts[i] for res in results for i in range(count) if res[i] is not None]
-    sb = [sum(sb[i * (count - 1): i * (count - 1) + count - 1]) for i in range(count)]
+    pts = [sum(res[:i] + res[i + 1:]) for res, i in zip(results, range(count))]
+    sb = [sum([r * p for r, p in zip(res, pts) if r is not None]) for res in results]
 
     l_rank, l_pl = len(str(count)), len(max(players, key=len))
     l_one_score = len(str(count))
@@ -26,40 +25,33 @@ def crosstable(players, results):
     for res in results:
         score.append([template_one_score.format(" " if el is None else int(el) if el != 0.5 else "=") for el in res])
 
-    list_sort = [dict(pts=pts[i], sb=sb[i], ind=i, name=players[i], res=score[i]) for i in range(count)]
-    sort_pts = list(sorted(list_sort, key=lambda el: el['pts'], reverse=True))
+    sort_pts = list(sorted([dict(pts=pts[i], sb=sb[i], ind=i, name=players[i], res=score[i]) for i in range(count)],
+                           key=lambda el: el['pts'], reverse=True))
     duplicate: bool = False
     start = 0
     for i in range(0, count):
         if not duplicate and i != count-1 and sort_pts[i + 1]['pts'] == sort_pts[i]['pts']:
-            duplicate = True
-            start = i
+            duplicate, start = True, i
         elif duplicate and (i == count-1 or sort_pts[i + 1]['pts'] != sort_pts[i]['pts']):
             sort_pts[start:i+1] = sort_by_sb(list(sorted(sort_pts[start:i+1], key=lambda el: el['sb'], reverse=True)),
                                              rank=start+1)
             duplicate = False
-            continue
         elif not duplicate:
             sort_pts[i]['rank'] = i + 1
     order = [rank['ind'] for rank in sort_pts]
     result = header
-    for rank in sort_pts:
-        el = rank
-        el['res'] = [el['res'][order[i]] for i in range(count)]
-        s = template_rank.format(el['rank']) + template.format(el["name"], " ".join(el['res'])) + \
-            template_score.format(el['pts'], el['sb']) + "\n"
-        result += s
-    return result.rstrip('\n')
+    for row in sort_pts:
+        row['res'] = [row['res'][order[i]] for i in range(count)]
+        result += template_rank.format(row['rank']) + template.format(row["name"], " ".join(row['res'])) + \
+            template_score.format(row['pts'], row['sb']) + "\n"
+    return result.rstrip()
 
 
 def sort_by_sb(arr: list, rank: int):
-    duplicate, start = False, 0
+    duplicate, start, rank_duplicate = False, 0, rank
     for i in range(len(arr)):
         if not duplicate and i != len(arr)-1 and arr[i]['sb'] == arr[i + 1]['sb']:
-            arr[i]['rank'] = " "
-            duplicate = True
-            start = i
-            rank_duplicate = rank
+            duplicate, start, rank_duplicate = True, i, rank
         elif duplicate and (i == len(arr)-1 or arr[i]['sb'] != arr[i+1]['sb']):
             arr[start:i + 1] = list(sorted(arr[start:i + 1], key=lambda el: el['name'].split(' ')[1]))
             for el in arr[start:i + 1]:
