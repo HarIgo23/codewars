9d429f203e80c43860710df3f3d1815f80417d29
HarIgo23 <igorn2304@gmail.com>
2020-10-14 12:21:49 +0300

    refactor tracer

--- 4 kyu/Counting inner calls in an unknown function.py
+++ 4 kyu/Counting inner calls in an unknown function.py
@@ -4,26 +4,18 @@ import sys
 
 def count_calls(func, *args, **kwargs):
     """Count calls in function func"""
-    tracer = Tracer(func)
+    tracer = Tracer()
     sys.settrace(tracer.my_tracer)
     rv = func(*args, **kwargs)
     return tracer.count, rv
 
 
 class Tracer:
-    count: int = 0
-    func_name: str
-    recursive: bool = False
-
-    def __init__(self, func: callable):
-        self.func_name = func.__name__
+    count: int = -1
 
     def my_tracer(self, frame, event, arg=None):
-        func_name = frame.f_code.co_name
-        if event == 'call' and (self.recursive or func_name != self.func_name):
+        if event == 'call':
             self.count += 1
-        elif func_name == self.func_name and not self.recursive:
-            self.recursive = True
         return self.my_tracer
 
 
